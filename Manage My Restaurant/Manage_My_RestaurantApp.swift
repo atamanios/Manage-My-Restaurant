339d8d37f0aa1f5de66d746b7a9c53efabe4851a
atamanios <atamandeniz@hotmail.com>
2023-04-02 22:21:43 +0400

    Initial Commit

---
+++ Manage My Restaurant/Manage_My_RestaurantApp.swift
@@ -0,0 +1,20 @@
+//
+//  Manage_My_RestaurantApp.swift
+//  Manage My Restaurant
+//
+//  Created by Ataman Deniz on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Manage_My_RestaurantApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
